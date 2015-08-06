93f3988bdd2e9ce7d3b09a960baec5baf9f954cd
Gavin Young <gavinyoung1994@gmail.com>
2015-08-06 15:15:43 -0400

    doesnt move normally

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ var app = express();
 var robot = require('robotjs');
 app.use(express.static( __dirname ));
 app.get('/send',function(req,res,next){
-	console.log(req.query)
+	console.log("rotate",req.query.rotate)
+	console.log("yspeed",req.query.up)
+	console.log('xspeed',req.query.side)
 	robot.moveMouseSmooth(req.query.x, req.query.y);
+	/*if(parseInt(req.query.rotate)>5){
+		robot.mouseClick();
+	}*/
+	// if(parseInt(req.query.up)>15){
+	// 	robot.keyTap('pageup');
+	// }else if(parseInt(req.query.up)<(-15)){
+	// 	robot.keyTap('pagedown');
+	// }
 	res.json("great");
 })
 app.listen(3000);
